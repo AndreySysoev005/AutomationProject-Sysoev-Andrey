8327727cbcf673e7970749023480fd741f827a8d
AndreySysoev005 <86248745+AndreySysoev005@users.noreply.github.com>
2021-10-14 00:08:39 +0700

    Update GoogleSearchTest.java

--- src/test/java/GoogleSearchTest.java
+++ src/test/java/GoogleSearchTest.java
@@ -2,16 +2,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GoogleSearchTest {
     @Test
-    public void test1() throws InterruptedException {
+    public void test1()  {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.ru/");
-        driver.findElement(By.name("q")).sendKeys("Hellow world", Keys.ENTER);
-        System.out.println(driver.findElement(By.tagName("h3")).getText());
-        System.out.println(driver.findElement(By.xpath("//h3[1]")).getText());
+        driver.manage().window().fullscreen();
+        driver.get("https://yandex.ru/");
+        driver.findElement(By.name("text")).sendKeys("Hello world", Keys.ENTER);
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        System.out.println(driver.findElement(By.xpath("//h2[1]")).getText());
+        driver.findElements(By.tagName("h2")).forEach(el -> {
+            System.out.println(el.getText());
+                });
+       Assert.assertEquals(driver.findElement(By.tagName("h2")).getText(),"Hello, world! — Википедия");
        driver.quit();
     }
 }
